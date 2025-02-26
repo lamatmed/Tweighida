da582066132f657121a0c37ca01f27af80605a41
lamatmed <lamat032025@gmail.com>
2025-02-26 20:51:57 +0100

    first commit

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -1,9 +1,9 @@
-import { Account, Client, Databases, Storage } from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
 export const client = new Client()
     .setEndpoint("https://cloud.appwrite.io/v1") // Remplace par ton endpoint
     .setProject("67bdb779002991c26e2c"); // Remplace par ton Project ID
 
-export const account = new Account(client);
+
 export const databases = new Databases(client);
 export const storage = new Storage(client);
