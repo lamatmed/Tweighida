c811133eec2c6493160736105fa0cc842269828f
lamatmed <lamat032025@gmail.com>
2025-02-26 22:38:44 +0100

    first commit dcd

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,12 +1,11 @@
 import { Client, Databases, Storage } from "appwrite";
 
 // Récupération des variables d'environnement
-const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
-const project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
-const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
-const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
-const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID;
-
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!;
+const project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT!;
+const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
+const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
+const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID!;
 
 // Vérification des variables d'environnement
 if (!endpoint || !project || !databaseId || !collectionId || !bucketId) {
