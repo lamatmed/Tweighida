4e27509c70f5c4793f7215938d6f5de5a11ebd6d
lamatmed <lamat032025@gmail.com>
2025-02-26 20:38:50 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-module.exports = {
-    env: {
-        NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-        NEXT_PUBLIC_APPWRITE_PROJECT: process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
-    },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
