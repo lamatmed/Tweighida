95195239fb0c5dff0c76db71541fafcb476d80cd
lamatmed <lamat032025@gmail.com>
2025-02-26 21:29:14 +0100

    first commit dcd

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     env: {
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-        NEXT_PUBLIC_APPWRITE_PROJECT: process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
-    },
+        NEXT_PUBLIC_APPWRITE_PROJECT: process.env.NEXT_PUBLIC_APPWRITE_PROJECT
+    }
 };
 
 export default nextConfig;
--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -1,9 +1,12 @@
 import { Client, Databases, Storage } from "appwrite";
 
-export const client = new Client()
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT) // Remplace par ton endpoint
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT); // Remplace par ton Project ID
+if (!process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || !process.env.NEXT_PUBLIC_APPWRITE_PROJECT) {
+    throw new Error("❌ Les variables d'environnement Appwrite ne sont pas définies !");
+}
 
+export const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT);
 
 export const databases = new Databases(client);
 export const storage = new Storage(client);
