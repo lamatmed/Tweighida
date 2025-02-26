3f5c64ee36b8f9f9abc8e145755297e56a1e3192
lamatmed <lamat032025@gmail.com>
2025-02-26 22:52:06 +0100

    first commit dcd

--- src/utils/appwrite.ts
+++ src/utils/appwrite.ts
@@ -1,16 +1,16 @@
 import { Client, Databases, Storage } from "appwrite";
 
-// Récupération des variables d'environnement
-const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!;
-const project = process.env.NEXT_PUBLIC_APPWRITE_PROJECT!;
-export const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
-export const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!;
-export const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID!;
+const client = new Client();
+client.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "")
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 
-
-// Initialisation du client Appwrite
-const client = new Client().setEndpoint(endpoint).setProject(project);
 const databases = new Databases(client);
 const storage = new Storage(client);
 
-export { client, databases, storage};
+// Vérification que les variables ne sont pas déclarées en double
+const databaseId = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID || "";
+const collectionId = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID || "";
+const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID || "";
+
+// Export unique
+export { client, databases, storage, databaseId, collectionId, bucketId };
